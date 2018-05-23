9b009db6c5fbbdcefea567633d2c08588f14da78
Bader <ibnbassem@gmail.com>
2018-05-23 19:31:14 -0400

    ohyeaaa

---
+++ server.js
@@ -0,0 +1,25 @@
+const lotion = require('lotion');
+const genesis = require.resolve("./genesis.json");
+
+const app = lotion({
+    initialState: {
+        foods: [ {sensor: "DHT11", temp: 57.2, time: "12:48 PM"}]
+    },
+    devMode: true,
+    peers: [],
+    logTendermint: false,
+    createEmptyBlocks: false,
+    p2pPort: 46658,
+    tendermintPort: 46657
+});
+
+app.use(function (state, tx) {
+    if (typeof tx.sensor === 'string' && typeof tx.temp === 'string' && typeof tx.time === 'string') {
+            state.foods.push({sensor: tx.sensor, temp: tx.temp, time: tx.time});
+    }
+});
+
+app.listen(3000).then(genesis => {
+    console.log("BLOCKCHAIN STARTED. WAITING FOR DA FOOD");
+   console.log(genesis.GCI, genesis.chain_id);
+});
